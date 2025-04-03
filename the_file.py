de1295c2679e4ff02d4f749e32ed1e06c5c83e8e
Midas123456789 <163131159+Midas123456789@users.noreply.github.com>
2025-04-03 21:02:54 +0200

    update

--- the_file.py
+++ the_file.py
@@ -1,5 +1,6 @@
 from wingcg import compute_cg
 import copy
+import matplotlib.pyplot as plt
 
 class Plane:
     def __init__(self):
@@ -19,19 +20,24 @@ class Plane:
 def add_points(plane, objects, masses, xcgs, reversed=False):
     _objects = objects[:]
     if reversed:
-        _objects = _objects.reverse()
-    for object in objects:
+        _objects.reverse()
+    cg = plane.get_cg()
+    mass = plane.get_mass()
+    xcgs.append(cg)
+    masses.append(mass)
+    for object in _objects:
+        new_mass = object[0]
+        new_pos = object[1]
+        plane.add_mass(new_mass, new_pos)
         cg = plane.get_cg()
         mass = plane.get_mass()
         xcgs.append(cg)
         masses.append(mass)
-        new_mass = object[0]
-        new_pos = object[1]
-        plane.add_mass(new_mass, new_pos)
         
         
 
-empty_plane = [(1000, 10)] #change
+empty_plane_mass = 1000
+empty_plane_cg = 10
 cargo = [(400, 4.45), (280, 22.5)] #(mass, pos)
 passenger_weight = 80
 rows = 18
@@ -39,10 +45,10 @@ start_passenger_comp = 5.94
 length_passenger_comp = 12.29
 seat_dist = length_passenger_comp / (rows)
 seat = seat_dist / 2 + start_passenger_comp
-passengers = []
+first_passengers = []
 
 for pos in range(18):
-    passengers.append((4*passenger_weight, seat))
+    first_passengers.append((2*passenger_weight, seat))
     seat += seat_dist
 
 # change all these 
@@ -58,6 +64,58 @@ front_loaded_cargo_positions = []
 front_loaded_cargo_masses = []
 rear_loaded_cargo_positions = []
 rear_loaded_cargo_masses = []
-front_loaded_cargo = Plane()
-rear_loaded_cargo = Plane()
-add_points()
+front_loaded_cargo_plane = Plane()
+front_loaded_cargo_plane.add_mass(empty_plane_mass, empty_plane_cg)
+rear_loaded_cargo_plane = copy.deepcopy(front_loaded_cargo_plane)
+add_points(front_loaded_cargo_plane, cargo, front_loaded_cargo_masses, front_loaded_cargo_positions)
+add_points(rear_loaded_cargo_plane, cargo, rear_loaded_cargo_masses, rear_loaded_cargo_positions, reversed=True)
+
+first_front_loaded_passengers_plane = copy.deepcopy(front_loaded_cargo_plane)
+first_rear_loaded_passengers_plane = copy.deepcopy(front_loaded_cargo_plane)
+first_rear_loaded_passengers_masses = []
+first_front_loaded_passengers_masses = []
+first_rear_loaded_passengers_positions = []
+first_front_loaded_passengers_positions = []
+add_points(first_front_loaded_passengers_plane, first_passengers, first_front_loaded_passengers_masses, first_front_loaded_passengers_positions)
+add_points(first_rear_loaded_passengers_plane, first_passengers, first_rear_loaded_passengers_masses, first_rear_loaded_passengers_positions, reversed=True)
+
+second_front_loaded_passengers_plane = copy.deepcopy(first_front_loaded_passengers_plane)
+second_rear_loaded_passengers_plane = copy.deepcopy(first_front_loaded_passengers_plane)
+second_rear_loaded_passengers_masses = []
+second_front_loaded_passengers_masses = []
+second_rear_loaded_passengers_positions = []
+second_front_loaded_passengers_positions = []
+add_points(second_front_loaded_passengers_plane, first_passengers, second_front_loaded_passengers_masses, second_front_loaded_passengers_positions)
+add_points(second_rear_loaded_passengers_plane, first_passengers, second_rear_loaded_passengers_masses, second_rear_loaded_passengers_positions, reversed=True)
+
+fuel_masses = []
+fuel_positions = []
+fueled_plane = copy.deepcopy(second_front_loaded_passengers_plane)
+add_points(fueled_plane, fuel, fuel_masses, fuel_positions)
+
+
+plt.plot(front_loaded_cargo_positions, front_loaded_cargo_masses, 
+         label="Front-loaded Cargo", marker='o')
+plt.plot(rear_loaded_cargo_positions, rear_loaded_cargo_masses, 
+         label="Rear-loaded Cargo", marker='o')
+
+plt.plot(first_front_loaded_passengers_positions, first_front_loaded_passengers_masses, 
+         label="Front-loaded Passengers", marker='^')
+plt.plot(first_rear_loaded_passengers_positions, first_rear_loaded_passengers_masses, 
+         label="Rear-loaded Passengers", marker='^')
+
+plt.plot(second_front_loaded_passengers_positions, second_front_loaded_passengers_masses, 
+         label="Front-loaded Passengers", marker='^')
+plt.plot(second_rear_loaded_passengers_positions, second_rear_loaded_passengers_masses, 
+         label="Rear-loaded Passengers", marker='^')
+
+plt.plot(fuel_positions, fuel_masses)
+
+# Configure labels and show
+plt.xlabel("Center of Gravity (m)")
+plt.ylabel("Total Mass (kg)")
+plt.title("CG vs Mass with Front/Rear Loading of Cargo + Passengers")
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.show()
