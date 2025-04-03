0305bdfd57105276dedf9fdb212c9469bf72e523
Midas123456789 <163131159+Midas123456789@users.noreply.github.com>
2025-04-03 19:41:40 +0200

    update

--- the_file.py
+++ the_file.py
@@ -10,3 +10,16 @@ class plane:
     def getcg(self):
         return sum(self.mass_list[i] * self.pos_list[i] for i in range(len(self.mass_list)))/sum(self.mass_list)
     
+cargo = [(400, 4.45), (280, 22.5)] #(mass, pos)
+passenger_weight = 80
+rows = 18
+start_passenger_comp = 5.94
+length_passenger_comp = 12.29
+seat_dist = length_passenger_comp / (rows)
+seat = seat_dist / 2 + start_passenger_comp
+passengers = []
+for pos in range(18):
+    passengers.append((4*passenger_weight, seat))
+    seat += seat_dist
+print(passengers)
+print(seat_dist)
